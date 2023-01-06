271e64d8d84fb3cce175af4e3c9937d6202a4121
Hwang Seong Taek <hst007@naver.com>
2023-01-06 09:35:55 +0900

    feat: handled useState

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,23 @@
+import React, { useState } from 'react';
+
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
 
   // 버튼 클릭 시 발생할 이벤트
   const clickHandler = () => {
-    console.log('Clicked!!');
+    setTitle('Updated!');
+    console.log(title);
   };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
